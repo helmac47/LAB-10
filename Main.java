20f81c4f3e5079062d142499711b3cf7fe9cee6f
helmac <>
2021-04-14 16:21:13 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,14 @@
+class Main {
+  public static void main(String[] args) {
+       
+//Cookie 1
+    Sugar tree = new Sugar("tree", false);
+    tree.cutOutShape("tree", 10);
+    tree.areBaked(350, 12);
+    tree.isDecorated();
+//Cookie 2
+   Sugar aCookie = new Sugar();
+    aCookie.isDecorated();
+  }
+}
+
